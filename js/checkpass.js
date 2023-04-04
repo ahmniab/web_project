d821c00647661b7948a88150f5145424b1c46f42
ahmniab <124299773+ahmniab@users.noreply.github.com>
2023-04-04 17:25:24 +0200

    add bootstrap alert to sign up

--- js/checkpass.js
+++ js/checkpass.js
@@ -1,22 +1,32 @@
+function msg(worning) {
+  const alerm = document.getElementById("alert");
+  alerm.style.display = "block";
+  alerm.innerHTML = " " ;
+  alerm.insertAdjacentHTML('beforeend', '<i class="fa-solid fa-circle-exclamation fa-beat"></i> ' + worning);
+}
 function checkPasswords() {
     var password = document.getElementById("password").value;
     var confirmPassword = document.getElementById("cnfrmpassword").value;
-    
-    if (confirmPassword == "" || password == "") {
-      alert("Your password is empty");
+    if ( password == "") {
+      msg("password is empty") ;
+
+    }
+    else if (confirmPassword == "" )
+    {
+      msg("Please confirm your password");
     }
     else{
 
       if (password.length < 6) {
-        alert("Password is less than 6");
+        msg("password is less than 6");
       }
       else if (password.length > 15)
       {
-        alert("Password is greater than 15");
+        msg("Password is greater than 15");
       }
       else{
         if (confirmPassword != password) {
-          alert("Confirm password doesn't equal password");
+          msg("Confirm password doesn't equal password");
         }
         
         else{
