179b13f9cf24a7ea850eafc17e66ad7c4ec35849
ahmniab <124299773+ahmniab@users.noreply.github.com>
2023-04-08 22:01:57 +0200

    more bakend  in index

--- connection.php
+++ connection.php
@@ -1,6 +1,6 @@
 <?php
-  $conn = new mysqli("sql7.freesqldatabase.com", "sql7611692", "7Af15l1R5u", "sql7611692");
-  // $conn = new mysqli("localhost", "root", "", "users");
+  // $conn = new mysqli("sql7.freesqldatabase.com", "sql7611692", "7Af15l1R5u", "sql7611692");
+  $conn = new mysqli("localhost", "root", "", "users");
   if (!$conn) {
     die("something went wrong");
   }
