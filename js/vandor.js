65d31e6867f5a82523e0a2cc6894a9d276b92440
ahmedhamed32 <129281128+ahmedhamed32@users.noreply.github.com>
2023-04-08 22:01:24 +0200

    hi

---
+++ js/vandor.js
@@ -0,0 +1,7 @@
+let toggler = document.querySelector(".toggle");
+let nav = document.querySelector("nav");
+let close = document.querySelector(".close");
+
+toggler.onclick =function(){
+    nav.classList.add("open")
+};
